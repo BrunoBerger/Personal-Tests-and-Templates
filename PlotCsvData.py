92849cab6866b8a3d5ca3c113a9bbc9d90846e8a
BrunoBerger <bruno.berger@stud.hshl.de>
2021-06-21 13:14:34 +0200

    data plotter tweak

--- PlotCsvData.py
+++ PlotCsvData.py
@@ -1,13 +1,15 @@
-import matplotlib.pyplot as plt
 import csv
+import matplotlib.pyplot as plt
 
+file = "data.csv"
 frameTimes = []
-with open("data.csv", "r") as frames:
+with open(file, "r") as frames:
     reader = csv.reader(frames, delimiter=',')
     next(reader, None)  # skip the headers
     for row in reader:
         frameTimes.append(float(row[1]))
 
+plt.figure(file)
 plt.plot(frameTimes)
 plt.ylabel("Frame times in ms")
 plt.xlabel("Frames")
